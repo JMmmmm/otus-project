57154d1e8e97743dbaa4289028d00e8ad20ee0b3
MatyushenkoEG <evgenijj.matyushenko@vseinstrumenti.ru>
2021-11-23 14:17:14 +0300

    "HW1 is completed"

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"fmt"
+
+	"golang.org/x/example/stringutil"
+)
+
 func main() {
-	// Place your code here.
+	reversedMessage := stringutil.Reverse("Hello, OTUS!")
+
+	fmt.Println(reversedMessage)
 }
